2f95cdb3b5c25a88c8febd00063b928997862ab7
Mohammed Boudad <19875946+matrixersp@users.noreply.github.com>
2020-01-07 12:08:22 +0100

    Allow CORS

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('express-async-errors');
 const express = require('express');
 
 const app = express();
+const cors = require('cors');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
 const error = require('./middleware/error');
@@ -20,6 +21,8 @@ mongoose
   })
   .then(() => console.log(`MongoDB connected on ${dbURI}`))
   .catch(err => console.log('Could not connect to MongoDB', err));
+
+app.use(cors());
 
 app.use(express.json());
 app.use(morgan('tiny'));
